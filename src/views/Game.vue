e415f50e2bf459ef8900826ec091d2fd1d062b05
Nicola Bosco <nicola.bosco@intesys.it>
2018-04-06 12:38:59 +0200

    first commit

---
+++ src/views/Game.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ChangeBoard/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ChangeBoard from '@/components/ChangeBoard.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    ChangeBoard,
+  },
+})
+export default class Home extends Vue {}
+</script>
--- tests/unit/ChangeBoard.spec.ts
+++ tests/unit/ChangeBoard.spec.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import { shallow } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import ChangeBoard from '@/components/ChangeBoard.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallow(HelloWorld, {
+    const wrapper = shallow(ChangeBoard, {
       propsData: { msg },
     });
     expect(wrapper.text()).to.include(msg);
